a1b8ccffcd79a604f7f52125794e0afb14dc11ec
amit_ <amitrai_13@yahoo.com>
2024-04-02 17:57:42 +0530

    removed flex, from main component layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,37 +33,38 @@ import {
 
 export default function Home() {
   return (
-    <main className='flex max-w-7xl mx-auto  font-poppins'>
+    <main className=' max-w-7xl mx-auto  font-poppins flex'>
       {/* <ThemePanel /> */}
       <div className='flex w-full flex-col'>
         <div className='flex w-full '>
           <Header />
         </div>
         <div className='mx-4 py-2'>
-          <Card className='flex '>
+          <Card className=' '>
             <div className='flex  flex-col '>
               <ProfileContainer />
               <ProjectContainer />
             </div>
-
-            <div className='py-10  '>
-              <div className='flex justify-center flex-col items-center'>
-                <div>
-                  <Text color='gray' size={'8'} weight={'medium'}>
-                    Lets work together.
-                  </Text>
-                </div>
-                <div className='text-gray-700  tracking-widest'>
-                  Creating unmatched product experience.
-                </div>
-                <div className='mt-5 flex gap-x-3'>
-                  <HireMe />
-                  <Email />
+            <div className='flex flex-col'>
+              <div className='py-10  '>
+                <div className='flex justify-center flex-col items-center'>
+                  <div>
+                    <Text color='gray' size={'8'} weight={'medium'}>
+                      Lets work together.
+                    </Text>
+                  </div>
+                  <div className='text-gray-700  tracking-widest'>
+                    Creating unmatched product experience.
+                  </div>
+                  <div className='mt-5 flex gap-x-3'>
+                    <HireMe />
+                    <Email />
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <Footer />
+              <Footer />
+            </div>
           </Card>
         </div>
       </div>
