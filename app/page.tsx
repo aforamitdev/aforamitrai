7c092f8c6dcd8bba4682cef4471e8558fca53ff0
aforamitdev <aforamitrai@gmail.com>
2026-04-27 10:37:32 +0530

    remove oss banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Hero } from "@/components/hero";
 import { About } from "@/components/about";
 import { Experience } from "@/components/experience";
-import { OpenSource } from "@/components/open-source";
+// import { OpenSource } from "@/components/open-source";
 import { CtaBanner } from "@/components/cta-banner";
 import { Skills } from "@/components/skills";
 import { Education } from "@/components/education";
@@ -13,7 +13,7 @@ export default function Home() {
       <Hero />
       <About />
       <Experience />
-      <OpenSource />
+      {/* <OpenSource /> */}
       <CtaBanner />
       <Skills />
       <Education />
